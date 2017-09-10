24432515e787cd4e3be316e3244795d15b4570e6
Pewpo <samulir@windowslive.com>
2017-09-10 13:31:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pewpo/tehtvt/vko1.java
@@ -0,0 +1,13 @@
+package com.example.pewpo.tehtvt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vko1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vko1);
+    }
+}
